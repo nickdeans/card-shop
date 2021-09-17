c35a0d8c4cbc82039073a7e3eb473f256336cc92
nickabramowicz@gmail.com <nickabramowicz@gmail.com>
2021-09-16 20:54:32 -0700

    created json data and card component

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,14 +1,14 @@
 import TopMain from './TopMain'
 import MidMain from './MidMain'
+import CardsMain from './CardsMain'
 
 const Main = () => {
     return (
-    <>
       <main>
         <TopMain />
         <MidMain />
+        <CardsMain />
       </main>
-    </>
     );
   }
   
---
+++ src/cardData.js
@@ -0,0 +1,36 @@
+const data = [
+    {
+        id: 1,
+        name: 'Jim Jones',
+        price: 49.99,
+        image: '',
+        text: 'Hello World'
+
+    },
+    {
+        id: 2,
+        name: 'Jimmy John',
+        price: 29.99,
+        image: '',
+        text: 'Hello People'
+
+    },
+    {
+        id: 3,
+        name: 'Jim Johnson',
+        price: 19.99,
+        image: '',
+        text: 'Hey People'
+
+    },
+    {
+        id: 4,
+        name: 'Jimmy Jones',
+        price: 9.99,
+        image: '',
+        text: 'Hello peoples'
+
+    },
+]
+
+export default data;
