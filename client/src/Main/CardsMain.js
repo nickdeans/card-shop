1a60c3101287ec3f233116ddfa2d57b7895b2517
nickabramowicz@gmail.com <nickabramowicz@gmail.com>
2022-02-15 21:21:01 -0800

    Used useEffect and useState to create dynamic rendering of treanding cards depending on window load and resize

--- client/src/Main/CardsMain.js
+++ client/src/Main/CardsMain.js
@@ -1,7 +1,8 @@
 import { useState, useContext } from 'react';
 // import brady from '../images/brady.jpeg'
 import ItemsCarousel from 'react-items-carousel';
-import { FaAngleLeft, FaAngleRight } from 'react-icons/fa'
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+import React from 'react';
 
 import { ProductsContext } from '../context/products-context';
 import SingleCard from './SingleCard'
@@ -13,7 +14,28 @@ const CardsMain = () => {
     const { products } = useContext(ProductsContext);    
     const allProducts = products.map(product => (
         <SingleCard { ...product } key={product.id} />
-      ));
+        ));
+
+    const [count, setCount] = useState(5)
+    
+    React.useEffect(() => {
+
+    function handleResize() {
+        // console.log('resized to: ', window.innerWidth, 'x', window.innerHeight)
+        if(window.innerWidth > 1315) {
+            setCount(5);
+        } else if(window.innerWidth < 1315 && window.innerWidth > 1075) {
+            setCount(4);
+        } else if(window.innerWidth < 1075 && window.innerWidth > 935) {
+            setCount(3);
+        } else if(window.innerWidth < 935) {
+            setCount(2);
+        } 
+    }
+
+    window.addEventListener('load', handleResize)
+    window.addEventListener('resize', handleResize)
+  })
 
     return (
         <>
@@ -21,7 +43,7 @@ const CardsMain = () => {
                 <ItemsCarousel
                     requestToChangeActive={setActiveItemIndex}
                     activeItemIndex={activeItemIndex}
-                    numberOfCards={5}
+                    numberOfCards={count}
                     gutter={20}
                     leftChevron={<button><FaAngleLeft /></button>}
                     rightChevron={<button><FaAngleRight /></button>}
